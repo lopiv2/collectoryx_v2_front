891457398967cbea146cac048353d11508e61809
IVAN-PC\Ivan <lopiv2@hotmail.com>
2022-08-08 17:48:24 +0200

    Arreglado el visor de feeds para las imagenes

--- src/pages/ViewFeeds.js
+++ src/pages/ViewFeeds.js
@@ -2,71 +2,66 @@ import React, { useEffect, useState } from "react";
 import Parser from "rss-parser";
 import { parse } from "himalaya";
 import "../styles/Dashboard.css";
-import xml from 'xml-js'
+import xml from "xml-js";
 import ConfigService from "../app/api/config.api";
 import {
-    TextField,
-    Button,
-    Grid,
-    Box,
-    Typography,
-    Card,
-    CardHeader,
-    CardContent,
-    CardMedia,
-    Avatar,
+  TextField,
+  Button,
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  Avatar,
 } from "@material-ui/core";
 import { useLocation } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
 export default function ViewFeeds(props) {
-    const parser = new Parser({
-        customFields: {
-            feed: ['image'],
-            item: [["media:content", "media:content", { keepArray: true }]],
-        },
+  const parser = new Parser({});
+  const [feedsList, setFeedsList] = useState([]);
+  const location = useLocation();
+
+  if (localStorage.getItem("user")) {
+    var user = localStorage.getItem("user");
+    var userData = JSON.parse(user);
+  }
+
+  /*const parseContentHTMLForImage = (html) => {
+    const json = parse(html);
+    json.map((item) => {
+      if (item.tagName === "img") {
+        image = item.attributes[0].value;
+      }
+      if (item.tagName === "p") {
+        for (let i = 0; i < item.children.length; i++) {
+          if (item.children[i].tagName === "img") {
+            console.log(item.children[i].attributes[2].value);
+            image = item.children[i].attributes[2].value;
+            //image = item.attributes[0].value;
+            //return item.children[i];
+          }
+        }
+      }
     });
-    const [feedsList, setFeedsList] = useState([]);
-    const location = useLocation();
-    let image = "";
+  };*/
 
-    if (localStorage.getItem("user")) {
-        var user = localStorage.getItem("user");
-        var userData = JSON.parse(user);
-    }
-
-    const parseContentHTMLForImage = (html) => {
-        const json = parse(html);
-        json.map((item) => {
-            if (item.tagName === "img") {
-                image = item.attributes[0].value;
-            }
-            if (item.tagName === "p") {
-                for (let i = 0; i < item.children.length; i++) {
-                    if (item.children[i].tagName === "img") {
-                        console.log(item.children[i].attributes[2].value);
-                        image = item.children[i].attributes[2].value
-                        //image = item.attributes[0].value;
-                        //return item.children[i];
-                    }
-                }
-            }
-        });
-    };
-
-    /*useEffect(() => {
+  /*useEffect(() => {
       console.log("Location changed");
     }, [location]);*/
 
-    useEffect(() => {
-        const feeds = ConfigService.getUserFeedsIDTitle(
-            userData.id,
-            location.state
-        ).then((response) => {
-            //console.log(response.data)
-            const myJSON = JSON.stringify(response.data);
-            console.log(myJSON.indexOf('img src='))
-            if (myJSON.includes("image")) {
+  useEffect(() => {
+    const feeds = ConfigService.getUserFeedsIDTitle(
+      userData.id,
+      location.state
+    ).then((response) => {
+      //console.log(response.data);
+      setFeedsList(response.data);
+      //const myJSON = JSON.stringify(response.data);
+      //console.log(myJSON.indexOf('img src='))
+      /*if (myJSON.includes("image")) {
                 //console.log("image")
             }
             if (response.data.includes("img")) {
@@ -77,14 +72,14 @@ export default function ViewFeeds(props) {
             var feed = parser.parseString(response.data).then((resp) => {
                 setFeedsList(resp.items);
                 //console.log(resp.items);
-            });
-        });
-    }, [location]);
+            });*/
+    });
+  }, [location]);
 
-    return (
-        <Box sx={{ display: "flex" }}>
-            <Grid>
-                {/*breadcrumbs.map(({
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Grid>
+        {/*breadcrumbs.map(({
           match,
           breadcrumb
         }) => (
@@ -93,55 +88,54 @@ export default function ViewFeeds(props) {
             <NavLink to={match.pathname}>{breadcrumb}</NavLink>
           </span>
         ))*/}
-                <Grid item xs={12}>
-                    <Typography variant="h4" component="h4">
-                        <FormattedMessage
-                            id="app.feed.title"
-                            values={{
-                                site: location.state ?? location.state,
-                            }}
-                        ></FormattedMessage>
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h4">
+            <FormattedMessage
+              id="app.feed.title"
+              values={{
+                site: location.state ?? location.state,
+              }}
+            ></FormattedMessage>
+          </Typography>
+        </Grid>
+        <Grid container spacing={10} className="container">
+          {feedsList.map((item, index) => (
+            <Grid item key={index}>
+              <Card
+                className="cardListItem-img-feed"
+                sx={{ height: 400, minWidth: 250, maxWidth: 10, boxShadow: 5 }}
+                ml={200}
+              >
+                <CardMedia
+                  height="140"
+                  component="img"
+                  image={item.image ?? item.image}
+                  className="cardListItem-img-feed"
+                  style={{ height: "auto" }}
+                ></CardMedia>
+                <CardHeader
+                  sx={{
+                    display: "flex",
+                    overflow: "hidden",
+                    "& .MuiCardHeader-content": {
+                      overflow: "hidden",
+                    },
+                  }}
+                  title={
+                    <Typography gutterBottom noWrap variant="h6" component="h4">
+                      {item.title}
                     </Typography>
-                </Grid>
-                <Grid container spacing={10} className="container">
-                    {feedsList.map((item, index) => (
-                        <Grid item key={index}>
-                            <Card
-                                sx={{ height: 400, minWidth: 250, maxWidth: 250, boxShadow: 5 }}
-                                ml={200}
-                            >
-                                {parseContentHTMLForImage(item.content)}
-                                <CardMedia
-                                    component="img"
-                                    image={image ?? image}
-                                    className="card-collection"
-                                    style={{ height: "auto" }}
-                                >
-                                    {/*console.log(parseContentHTMLForImage(item.content))*/}
-                                </CardMedia>
-                                <CardHeader
-                                    sx={{
-                                        display: "flex",
-                                        overflow: "hidden",
-                                        "& .MuiCardHeader-content": {
-                                            overflow: "hidden",
-                                        },
-                                    }}
-                                    title={
-                                        <Typography gutterBottom noWrap variant="h6" component="h4">
-                                            {item.title}
-                                        </Typography>
-                                    }
-                                    titleTypographyProps={{ noWrap: true }}
-                                />
-                                <CardContent>
-                                    <Typography variant="body2" color="initial"></Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
+                  }
+                  titleTypographyProps={{ noWrap: true }}
+                />
+                <CardContent>
+                  <Typography style={{wordWrap: "break-word"}} variant="body2" color="initial"></Typography>
+                </CardContent>
+              </Card>
             </Grid>
-        </Box>
-    );
+          ))}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
